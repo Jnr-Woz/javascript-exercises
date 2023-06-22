3d7a72c2c1d38aec8255b436e04635bdfcc5e9fc
w0z <134621857+Jnr-Woz@users.noreply.github.com>
2023-06-22 18:32:38 +0100

    Complete 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // create a reversedString that stores the string, it is = "" starting out
+    // create a loop that goes thru the string starting from its (length - 1)
+    // concatenate each char to reversedString using slice()
+    // retrurn the reversedString at the end
+    // done
+    let reversedString = "";
 
-};
-
+    for (i = string.length - 1; i >= 0; i--) {
+        reversedString += string[i];
+    }
+    
+    return reversedString;
+}
 // Do not edit below this line
 module.exports = reverseString;
